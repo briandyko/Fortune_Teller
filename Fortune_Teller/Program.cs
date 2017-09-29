e39a4e75e58fdeccc0d6dd7c6db9781ba879e520
briandyko <briandyko@gmail.com>
2017-09-29 14:07:00 -0400

    Halfway thru Part 2

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -14,42 +14,91 @@ namespace Fortune_Teller
             string invitation = Console.ReadLine().ToLower();
 
             //string answerInvitation = "yes";
-               
+
             //    if (invitation == answerInvitation)
             {
                 Console.WriteLine("What is your first name?");
                 string fname = Console.ReadLine().ToLower();
             }
-           
-                //else
+
+            //else
             //{
             //    Console.WriteLine("you have made a very bad choice, my friend, the trolls says before he eats you. game over.");
             //}
-            
+
 
             Console.WriteLine("What is your last name?");
             string lname = Console.ReadLine().ToLower();
 
             Console.WriteLine("What is your age?");
-            string age = Console.ReadLine().ToLower();
+            int age = int.Parse(Console.ReadLine());
 
             Console.WriteLine("What is the number of your birth month?");
             int birthMonth = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("What is your favorite ROYGBIB color? If you do not know what ROYGBIV colors are, enter \"Help\".");
-            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite ROYGBIV color? If you do not know what ROYGBIV colors are, enter \"Help\".");
+            string favColor = Console.ReadLine().ToLower();
 
-            if (favColor=="Help")
-                {
+            int numSiblings;
+
+            if (favColor == "help")
+            {
                 Console.WriteLine("Red, Orange, Yellow, Green, Blue, Indigo, Violet");
+                Console.WriteLine("What is your favorite ROYGBIV color?");
+                string favColorPostHelp = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("What is the number of siblings that you have?");
+            numSiblings = int.Parse(Console.ReadLine());
+
+
+            //this is believe is the end of part 1, at least for now.
+
+            Console.WriteLine("Here is your fortune, mortal!");
+            int retireAge = age % 2;
+
+            if (retireAge == 0)
+            {
+                Console.WriteLine("You will retire in 35 years.");
             }
             else
             {
-                Console.WriteLine("What is the number of siblings that you have?");
-                string numSiblings = Console.ReadLine();
-
-     //this is believe is the end of part 1, at least for now.
+                Console.WriteLine("You will retire in 4 years.");
             }
+
+            if (numSiblings == 0)
+            {
+                Console.WriteLine("You will have a vacation home in Turks and Cacos, but mostly Cacos");
+            }
+            else if (numSiblings == 1)
+            {
+                Console.WriteLine("You will have a vacation home on Kelly's Island.");
+            }
+            else if (numSiblings == 2)
+            {
+                Console.WriteLine("You will have a vaction home on Jupiter");
+            }
+            else if (numSiblings == 3)
+            {
+                Console.WriteLine("You will have a vacation home in Maui");
+            }
+            else if (numSiblings > 3)
+            {
+                Console.WriteLine("You will have a vacation home in Phoenix");
+            }
+            else
+                {
+                Console.WriteLine("You will have a vacation in the eteranal fires of Hades!");
+                }
+           
+            
+
+               
+
+
+       
+
+
         }
     }
 }
