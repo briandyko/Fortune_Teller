0b0cf6618830aff19551d390003a2fffe3a24931
briandyko <briandyko@gmail.com>
2017-09-29 12:25:39 -0400

    Initial Commit

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -10,7 +10,46 @@ namespace Fortune_Teller
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("You are driving home from work after a busy, yet productive Friday. You are listening to some cool tunes, when all of a sudden the radio goes out. Then the engine turns off. You get out of your car and suddenly find yourself in a forest. You get out without much trouble, but approach a bridge. A troll comes from underneath and tells you this is his bridge, and the price to cross it is having your fortune read. Will you let him read your fortune?.");
+            string invitation = Console.ReadLine().ToLower();
 
+            //string answerInvitation = "yes";
+               
+            //    if (invitation == answerInvitation)
+            {
+                Console.WriteLine("What is your first name?");
+                string fname = Console.ReadLine().ToLower();
+            }
+           
+                //else
+            //{
+            //    Console.WriteLine("you have made a very bad choice, my friend, the trolls says before he eats you. game over.");
+            //}
+            
+
+            Console.WriteLine("What is your last name?");
+            string lname = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine().ToLower();
+
+            Console.WriteLine("What is the number of your birth month?");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite ROYGBIB color? If you do not know what ROYGBIV colors are, enter \"Help\".");
+            string favColor = Console.ReadLine();
+
+            if (favColor=="Help")
+                {
+                Console.WriteLine("Red, Orange, Yellow, Green, Blue, Indigo, Violet");
+            }
+            else
+            {
+                Console.WriteLine("What is the number of siblings that you have?");
+                string numSiblings = Console.ReadLine();
+
+     //this is believe is the end of part 1, at least for now.
+            }
         }
     }
 }
